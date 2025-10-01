d9fb58d533a5c4dc2c4b82f4fb3b6bb9c6bb6d31
vkiller <ebangren@gmail.com>
2025-10-01 21:21:03 +0800

    add multi entry page

---
+++ her/her/Silver.swift
@@ -0,0 +1,16 @@
+//
+//  Silver.swift
+//  Her
+//
+//  Created by dev on 2025/10/1.
+//
+
+import SwiftUI
+
+struct Silver: View {
+    var body: some View {
+        Text("Hello Silver")
+            .font(.largeTitle)
+            .padding()
+    }
+}
--- her/her/herApp.swift
+++ her/her/herApp.swift
@@ -5,8 +5,42 @@
 //  Created by dev on 2025/9/30.
 //
 
+//import SwiftUI
+//import SwiftData
+//
+//@main
+//struct HerApp: App {
+//    var sharedModelContainer: ModelContainer = {
+//        let schema = Schema([
+//            Item.self,
+//        ])
+//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+//
+//        do {
+//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+//        } catch {
+//            fatalError("Could not create ModelContainer: \(error)")
+//        }
+//    }()
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//        .modelContainer(sharedModelContainer)
+//    }
+//}
+
+
 import SwiftUI
 import SwiftData
+
+enum Template: String {
+    case Silver
+    case Platinum
+    case Diamond
+    case Default
+}
 
 @main
 struct HerApp: App {
@@ -23,9 +57,21 @@ struct HerApp: App {
         }
     }()
 
+    // 这里假设从存储里读到 Template 的值
+    var selectedTemplate: Template = .Default
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            switch selectedTemplate {
+            case .Silver:
+                Silver()
+            case .Platinum:
+                Platinum()
+            case .Diamond:
+                Diamond()
+            case .Default:
+                DefaultView()
+            }
         }
         .modelContainer(sharedModelContainer)
     }
